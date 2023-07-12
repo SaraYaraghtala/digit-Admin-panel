cbe745f2e2b6613e4f0a8da6416488e42208bc0f
Sara Yaraghtala <sarahyaraghtala@gmail.com>
2023-07-12 23:36:28 +0200

    Modify code in React component

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,61 +1,121 @@
-import * as React from 'react';
+// import * as React from 'react';
+import React, { useEffect, useState } from "react";
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
+import { set } from "react-hook-form";
 const columns = [
     { field: 'id', headerName: 'ID', width: 90 },
     {
-      field: 'firstName',
-      headerName: 'First name',
+      field: 'title',
+      headerName: 'title',
       width: 150,
       editable: true,
     },
     {
-      field: 'lastName',
-      headerName: 'Last name',
-      width: 150,
-      editable: true,
-    },
-    {
-      field: 'age',
-      headerName: 'Age',
+      field: 'oldprice',
+      headerName: 'oldprice',
       type: 'number',
       width: 110,
       editable: true,
     },
     {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+      field: 'price',
+      headerName: 'price',
+      type: 'number',
+      width: 110,
+      editable: true,
     },
+    {
+      field: 'image',
+      headerName: 'image',
+      width: 110,
+      editable: true,
+      renderCell: (params) => (
+        <img src={ import.meta.env.VITE_BASE_URL +
+          ((params.value.data != null &&
+           params.value.data?.attributes.url) ||
+            "")} alt="Product" style={{ width: 50,margin:"6px" }} />
+      ),
+    },
+    {
+      field: 'discount',
+      headerName: 'discount',
+      type: 'number',
+      width: 110,
+      editable: true,
+    },
+    {
+      field: 'showinbaner',
+      headerName: 'showinbaner',
+      width: 110,
+      editable: true,
+    },
+    {
+      field: 'showincarousel',
+      headerName: 'showinbaner',
+      width: 110,
+      editable: true,
+    },
+    // {
+    //   field: 'fullName',
+    //   headerName: 'Full name',
+    //   description: 'This column has a value getter and is not sortable.',
+    //   sortable: false,
+    //   width: 160,
+    //   valueGetter: (params) =>
+    //     `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    // },
   ];
   
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  ];
+  // const rows = [
+  //   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  //   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  //   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  //   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  //   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  //   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  //   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  //   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  //   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  // ];
   
 
 const ProductList = () => {
+  const [productData, setProductData] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+   fetch( import.meta.env.VITE_BASE_URL +
+    "/api/products?populate=*" ,
+  {
+    headers: { Authorization: "bearer " + import.meta.env.VITE_API_KEY }
+  })
+    .then(response => response.json())
+    .then(data =>  {
+    const tempRow= data.data.map((item)=>{
+        return(
+          {id:item.id,...item.attributes}
+        )
+    })
+    setProductData(tempRow)
+    console.log(tempRow)
+    })
+    .catch(error => console.log(error));
+  };
+
+  
     return (
         <Box sx={{ height: 400, width: '100%' }}>
         <DataGrid
-          rows={rows}
+          rows={productData}
           columns={columns}
           initialState={{
             pagination: {
               paginationModel: {
-                pageSize: 5,
+                pageSize: 6,
               },
             },
           }}

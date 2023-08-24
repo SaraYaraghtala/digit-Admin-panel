152d2b2f36e0f9629582166b3a6657f9e82acd4c
Sara Yaraghtala <sarahyaraghtala@gmail.com>
2023-08-24 21:00:51 +0200

    create new pages for orders

---
+++ src/Pages/Orders.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import OrderList from '../components/OrderList';
+
+const Orders = () => {
+    return (
+        <>
+           <OrderList /> 
+        </>
+    );
+};
+
+export default Orders;

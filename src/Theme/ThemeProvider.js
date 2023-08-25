d8ca236f00ab2012a6f28d0d11842825a0a5408e
Sara Yaraghtala <sarahyaraghtala@gmail.com>
2023-08-25 22:58:39 +0200

    create new folder is called Theme for theme

---
+++ src/Theme/ThemeProvider.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#EE384E",
+      light: "#F8EBED",
+      dark: "#AB717D",
+      contrastText: "#fff",
+    },
+  },
+});
+export default theme;

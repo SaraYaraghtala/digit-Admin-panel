1a0f202deb924b5fcb8f14c61a836ede19c0ad40
Sara Yaraghtala <sarahyaraghtala@gmail.com>
2023-09-04 23:25:19 +0200

    [Fix]:change style in user component

--- src/components/Users/usersTable/UserTable.jsx
+++ src/components/Users/usersTable/UserTable.jsx
@@ -13,11 +13,11 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
-  createData('Sarah Yaraghtala', 159, 6.0, 24, 4.0),
-  createData('Samira Gholami', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Sarah Yaraghtala',"Admin", "srahyaragh@gmail.com","online","20.2.2022"),
+  createData('Samira Gholami',"user", "SamiraGholami@gmail.com","online","20.2.2022"),
+  createData('Eclar Madies', "MAneger", "EclarMadies@gmail.com","online","20.2.2022"),
+  createData('Calson Wankok',"user", "srahyaragh@gmail.com","online","20.2.2022"),
+  createData('Gina Wondensi', "user", "srahyaragh@gmail.com","online","20.2.2022"),
 ];
 
 const UserTable =() => {
@@ -27,11 +27,11 @@ const UserTable =() => {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>AUTHOR</TableCell>
+            <TableCell align="right">FUNCTION</TableCell>
+            <TableCell align="right">EMAIL</TableCell>
+            <TableCell align="right">STATUS</TableCell>
+            <TableCell align="right">DATE</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
